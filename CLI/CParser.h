f50552070c271ea88b8b7c8b7114b1f80571b072
SoghomonyanElina <elinasogh@DESKTOP-VPQOFK7>
2024-10-15 12:21:48 +0400

    Created files

---
+++ CLI/CParser.h
@@ -0,0 +1,17 @@
+#ifndef CPARSER_HPP
+#define CPARSER_HPP
+#include "Tokenizer.h"
+#include "SyntaxAnalyser.h"
+#include <iostream>
+
+class CParser {
+    public:
+      CParser(std::string cmd);
+      ~CParser();
+
+
+
+
+};
+
+#endif //CPARSER_HPP

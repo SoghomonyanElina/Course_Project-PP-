d881e180d66eb9325e2a3e71ad9b4e02dc517520
SoghomonyanElina <144264461+SoghomonyanElina@users.noreply.github.com>
2024-12-24 09:42:28 +0400

    Add files via upload

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -6,16 +6,16 @@ Editor& Editor::getInstance() {
 }
 
 void Editor::process(std::shared_ptr<IAction> action) {
-    stack_undo.push(action->Do);
+    stack_undo.push(action->Do());
     stack_redo.clear();
 }
 
-std::shared_ptr<IAction> Editor::undo() {
-    stack_redo.push(stack_undo.top()->Do);
+void Editor::undo() {
+    stack_redo.push(stack_undo.top()->Do());
     stack_undo.pop();
 }
 
-std::shared_ptr<IAction> Editor::redo() {
+void Editor::redo() {
     stack_undo.push(stack_redo.top()->Do());
     stack_redo.pop();
 }
--- Editor/Editor.h
+++ Editor/Editor.h
@@ -5,14 +5,15 @@
 
 class Editor {
     public:
-      static Editor& getInstance();
-      void process(std::shared_ptr<IAction> action);
-      std::shared_ptr<IAction> undo();
-      std::shared_ptr<IAction> redo();
-    private:
+      //static Editor& getInstance();
       Editor() = default;
-      Editor(const Editor& other) = delete;
-      Editor& operator=(const Editor& other) = delete;
+      void process(std::shared_ptr<IAction> action);
+      void undo();
+      void redo();
+    private:
+      //Editor() = default;
+      //Editor(const Editor& other) = delete;
+      //Editor& operator=(const Editor& other) = delete;
       std::stack<std::shared_ptr<IAction>> stack_undo;
       std::stack<std::shared_ptr<IAction>> stack_redo;
 };
